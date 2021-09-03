7b4860bdf4bc55fd5e65fc71d3bba19127a1b92a
Gopal Krushna Parida <78479101+Inertia123@users.noreply.github.com>
2021-09-03 20:12:19 +0530

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,7 @@
+
+function toggle(){
+    const toggle = document.querySelector('.toggle');
+    const banner = document.querySelector('.banner');
+    toggle.classList.toggle('active')
+    banner.classList.toggle('active')
+}
